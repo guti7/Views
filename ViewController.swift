938cfe0cc088f6ef57888bedc152cd6f4483e979
José Gutiérrez <guti7@users.noreply.github.com>
2017-01-25 22:53:12 -0800

    add black subview

--- ViewController.swift
+++ ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet var redView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    
+    // MARK: - App lifecyle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create a view and add as a subview
+        let frame1 = CGRect(x: 50, y: 50, width: 50, height: 50)
+        let purpleView = UIView(frame: frame1)
+        purpleView.backgroundColor = UIColor.purple
+        
+        view.addSubview(purpleView)
+        
+        // add a subview to purpleView
+        let frame2 = CGRect(x: 10, y: 10, width: 30, height: 30)
+        let blackView = UIView(frame: frame2)
+        blackView.backgroundColor = .black
+        
+        purpleView.addSubview(blackView)
+        
+        redView.backgroundColor = .orange
+        
+        print(view.superview)
+        print(purpleView.superview as Any)
+        //print(view.subviews)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
